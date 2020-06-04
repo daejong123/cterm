f4f4c35fa1e478e754073d26347c3e790474a9c3
lindazhong <lindazhong@youzan.com>
2020-06-04 21:17:42 +0800

    fix: 详情支持添加item

--- data.go
+++ data.go
@@ -1,31 +1,38 @@
 package cterm
 
+import (
+	"fmt"
+)
+
 type ContentListSourceDataType struct {
 	Title string
 	ID    int
-	Msg   []string
+	Msg   *[]string
 }
 
 type IndexListSourceDataType struct {
 	Name        string
 	ID          int
 	Desc        string
-	ContentList []ContentListSourceDataType
+	ContentList *[]ContentListSourceDataType
 }
 
 func FetchData() {
 	indexListSourceData = readFromDataFile()
-	// indexListSourceData = []IndexListSourceDataType{
-	// 	{
-	// 		Name: "工作",
-	// 		ID:   1,
-	// 		Desc: "hz的生活",
-	// 		ContentList: []ContentListSourceDataType{
-	// 			{"地址", 1, []string{"江干", "西湖"}},
-	// 			{"公司", 2, []string{"mfe", "yz"}},
-	// 		},
-	// 	},
-	// 	{Name: "生活", ID: 2, Desc: "yz的生活", ContentList: []ContentListSourceDataType{{"同事", 1, []string{"morgan", "fantasy"}}}},
-	// 	{Name: "休闲", ID: 3, Desc: "hz的生活", ContentList: []ContentListSourceDataType{{"吃饭地", 1, []string{"大东北", "多伦多", "大渝火锅"}}}},
-	// }
+	fmt.Println(indexListSourceData)
+	if len(indexListSourceData) == 0 {
+		indexListSourceData = []IndexListSourceDataType{
+			{
+				Name: "工作",
+				ID:   1,
+				Desc: "hz的生活",
+				ContentList: &[]ContentListSourceDataType{
+					{"地址", 1, &[]string{"江干", "西湖"}},
+					{"公司", 2, &[]string{"mfe", "yz"}},
+				},
+			},
+			{Name: "生活", ID: 2, Desc: "yz的生活", ContentList: &[]ContentListSourceDataType{{"同事", 1, &[]string{"morgan", "fantasy"}}}},
+			{Name: "休闲", ID: 3, Desc: "hz的生活", ContentList: &[]ContentListSourceDataType{{"吃饭地", 1, &[]string{"大东北", "多伦多", "大渝火锅"}}}},
+		}
+	}
 }
--- ui.go
+++ ui.go
@@ -1,6 +1,8 @@
 package cterm
 
 import (
+	"strconv"
+
 	"github.com/atotto/clipboard"
 	"github.com/gdamore/tcell"
 	"github.com/rivo/tview"
@@ -15,8 +17,8 @@ var (
 	contentPageRight    *tview.Flex
 	contentList         *tview.List
 	msgList             *tview.List
-	currentContentList  []ContentListSourceDataType
-	currentContentData  []string
+	currentContentData  *[]string
+	currentContentList  *[]ContentListSourceDataType
 	indexListSourceData []IndexListSourceDataType
 )
 
@@ -28,30 +30,33 @@ func Draw(app *tview.Application) {
 		AddPage("indexPage", indexPage, true, true).
 		AddPage("contentPage", contentPage, true, false)
 
-	app.SetRoot(pages, true).EnableMouse(true)
+	app.SetRoot(pages, true).EnableMouse(false)
 }
 
 func initIndexPage(app *tview.Application) {
-	indexList = tview.NewList()
+	indexList = tview.NewList().
+		SetSelectedBackgroundColor(tcell.ColorDeepSkyBlue).
+		SetSecondaryTextColor(tcell.ColorGray)
 	indexList.
 		SetBorder(true).
 		SetBorderColor(tcell.Color20).
-		SetBorderAttributes(tcell.AttrUnderline).
+		SetBorderPadding(5, 5, 5, 5).
+		SetBorderAttributes(tcell.AttrNone).
 		SetTitle("  Welcome to New World  ").
 		SetTitleColor(tcell.ColorPink)
 
 	for _, v := range indexListSourceData {
-		indexList.AddItem(v.Name, v.Desc, 0, nil)
+		indexList.AddItem(strconv.Itoa(v.ID)+"、"+v.Name, v.Desc, 0, nil)
 	}
 
 	indexList.SetSelectedFunc(func(index int, _ string, _ string, _ rune) {
 		contentList.Clear()
-		currentContentList = indexListSourceData[index].ContentList
-		for _, v := range currentContentList {
-			contentList.AddItem(v.Title, "", 0, nil)
+		selectedIndexSourceData := indexListSourceData[index]
+		currentContentList = selectedIndexSourceData.ContentList
+		for _, v := range *currentContentList {
+			contentList.AddItem(strconv.Itoa(v.ID)+"、"+v.Title, "", 0, nil)
 		}
 		pages.SwitchToPage("contentPage")
-
 	})
 
 	indexPage = tview.NewFlex()
@@ -60,17 +65,17 @@ func initIndexPage(app *tview.Application) {
 	indexPage.AddItem(
 		tview.NewFlex().
 			SetDirection(tview.FlexRow).
-			AddItem(tview.NewBox(), 0, 2, false).
+			AddItem(tview.NewBox(), 0, 1, false).
 			AddItem(indexList, 0, 3, true).
-			AddItem(tview.NewBox(), 0, 2, false),
+			AddItem(tview.NewBox(), 0, 1, false),
 		0, 1, true)
 	indexPage.AddItem(tview.NewBox(), 0, 1, false)
 }
 
 func initContentPage(app *tview.Application) {
 
-	contentList = tview.NewList().SetSelectedBackgroundColor(tcell.ColorPink)
-	contentList.SetBorderPadding(0, 0, 0, 0)
+	contentList = tview.NewList().
+		SetSelectedBackgroundColor(tcell.ColorDeepSkyBlue)
 
 	contentList.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 		switch event.Rune() {
@@ -82,8 +87,9 @@ func initContentPage(app *tview.Application) {
 				inputValue := inputContentView.GetText()
 				if key == tcell.KeyEnter {
 					if 0 != len(inputValue) {
-						currentContentList = append(currentContentList, ContentListSourceDataType{Title: inputValue, ID: len(currentContentList) + 1})
+						*currentContentList = append(*currentContentList, ContentListSourceDataType{Title: inputValue, ID: len(*currentContentList) + 1, Msg: &[]string{}})
 						contentList.AddItem(inputValue, "", 0, nil)
+						writeToDatafile(indexListSourceData)
 					}
 					contentPageLeft.RemoveItem(inputContentView)
 					app.SetFocus(contentList)
@@ -95,30 +101,60 @@ func initContentPage(app *tview.Application) {
 		return event
 	})
 
-	msgList = tview.NewList().SetSelectedBackgroundColor(tcell.ColorBlue)
-	msgList.SetBorderPadding(1, 1, 1, 1)
-
 	contentList.SetSelectedFunc(func(index int, _ string, _ string, _ rune) {
 		msgList.Clear()
 		app.SetFocus(msgList)
-		currentContentData = currentContentList[index].Msg
-		for _, v := range currentContentData {
+		currentContentData = (*currentContentList)[index].Msg
+		for _, v := range *currentContentData {
 			msgList.AddItem(v, "", 0, nil)
-			contentPageRight.AddItem(msgList, 0, 1, true)
 		}
+		contentPageRight.AddItem(msgList, 0, 1, true)
 	})
 
+	msgList = tview.NewList().
+		SetSelectedBackgroundColor(tcell.ColorDeepSkyBlue)
 	msgList.SetSelectedFunc(func(index int, _ string, _ string, _ rune) {
-		clipboard.WriteAll(currentContentData[index])
+		clipboard.WriteAll((*currentContentData)[index])
+	})
+	msgList.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		switch event.Rune() {
+		case 'i':
+			inputContentView := tview.NewInputField()
+			inputContentView.SetLabel("请输入内容：")
+			inputContentView.SetFieldWidth(0)
+			inputContentView.SetDoneFunc(func(key tcell.Key) {
+				inputValue := inputContentView.GetText()
+				if key == tcell.KeyEnter {
+					if 0 != len(inputValue) {
+						*currentContentData = append(*currentContentData, inputValue)
+						msgList.AddItem(inputValue, "", 0, nil)
+						writeToDatafile(indexListSourceData)
+					}
+					contentPageRight.RemoveItem(inputContentView)
+					app.SetFocus(msgList)
+				}
+			})
+			contentPageRight.AddItem(inputContentView, 10, 1, true)
+			app.SetFocus(inputContentView)
+		}
+		return event
 	})
 
 	contentPageLeft = tview.NewFlex()
 	contentPageRight = tview.NewFlex()
 
-	contentPageLeft.SetDirection(tview.FlexRow).SetBorder(true).SetTitle(" 类目 [ enter 查看内容， q退出 ]")
+	contentPageLeft.
+		SetDirection(tview.FlexRow).
+		SetBorder(true).
+		SetBorderPadding(0, 0, 2, 2).
+		SetTitle(" 类目 [ enter 查看内容， q退出 ]")
 	contentPageLeft.AddItem(contentList, 0, 1, true)
 
-	contentPageRight.SetBorder(true).SetTitle(" 内容 [ enter 复制内容, q 退出 ]")
+	contentPageRight.
+		SetDirection(tview.FlexRow).
+		SetBorderPadding(0, 0, 2, 2).
+		SetBorder(true).
+		SetTitle(" 内容 [ enter 复制内容, q 退出 ]")
 
 	contentPage = tview.NewFlex()
 	contentPage.AddItem(contentPageLeft, 0, 1, true).AddItem(contentPageRight, 0, 1, false)
